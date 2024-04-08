42351492da3c631b4f7c59caf14f80927c5f97ed
vinzgdec <va.alfaro@greatdealscorp.com>
2024-04-08 12:08:48 +0800

    Remove empty operators and organize import statements

--- dags/shopee_dbt/shopee_finance_transactions_dag.py
+++ dags/shopee_dbt/shopee_finance_transactions_dag.py
@@ -1,13 +1,12 @@
+
 from datetime import datetime
 from pathlib import Path
 
 from airflow.decorators import dag
-from airflow.operators.empty import EmptyOperator
 
-from cosmos import ExecutionConfig
-from cosmos import DbtTaskGroup, ProjectConfig
-from cosmos import ProfileConfig
+from cosmos import DbtTaskGroup, ExecutionConfig, ProjectConfig, ProfileConfig
 from cosmos.profiles import SnowflakeUserPasswordProfileMapping
+
 
 gdec_dbt_path = Path("/opt/airflow/dbt/gdec_dbt")
 dbt_executable = Path("/opt/airflow/dbt_venv/bin/dbt")
@@ -35,21 +34,14 @@ dbt_snowflake = ProfileConfig(
     tags=["shopee"],
 )
 def dag_shopee_finance_transactions() -> None:
-    """
-    The simplest example of using Cosmos to render a dbt project as a TaskGroup.
-    """
-    pre_dbt = EmptyOperator(task_id="pre_dbt")
-
     shopee_finance_transactions = DbtTaskGroup(
         group_id="shopee_finance_transactions",
         project_config=ProjectConfig(gdec_dbt_path),
         profile_config=dbt_snowflake,
-        execution_config=venv_execution_config,
+        execution_config=venv_execution_config
     )
 
-    post_dbt = EmptyOperator(task_id="post_dbt")
-
-    pre_dbt >> shopee_finance_transactions >> post_dbt
+    shopee_finance_transactions
 
 
 dag_shopee_finance_transactions()
