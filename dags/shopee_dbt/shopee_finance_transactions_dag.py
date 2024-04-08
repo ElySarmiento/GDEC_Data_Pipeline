56286d8c2a380b483307804cc8bcd49a1b60e7c0
vinzgdec <va.alfaro@greatdealscorp.com>
2024-04-08 10:52:58 +0800

    Revert back shopee_finance_transactions_dag to original

--- dags/shopee_dbt/shopee_finance_transactions_dag.py
+++ dags/shopee_dbt/shopee_finance_transactions_dag.py
@@ -1,31 +1,12 @@
+from datetime import datetime
 from pathlib import Path
 
 from airflow.decorators import dag
-from airflow.operators.empty import EmptyOperator
-
-from cosmos import ExecutionConfig
-from cosmos import DbtTaskGroup, ProjectConfig
-from cosmos import ProfileConfig
+from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig, ExecutionConfig
 from cosmos.profiles import SnowflakeUserPasswordProfileMapping
 
-GDEC_DBT_PATH = Path("/opt/airflow/dbt/gdec_dbt")
+GDEC_DBT_PATH =  Path("/opt/airflow/dbt/gdec_dbt")
 DBT_EXECUTABLE = Path("/opt/airflow/dbt_venv/bin/dbt")
-
-venv_execution_config = ExecutionConfig(
-    dbt_executable_path=str(DBT_EXECUTABLE),
-)
-
-dbt_snowflake = ProfileConfig(
-    profile_name="dbt_snowflake",
-    target_name="dev",
-    profile_mapping=SnowflakeUserPasswordProfileMapping(
-        conn_id="dbt_snowflake",
-        profile_args={
-            "database": "DEV_DB",
-            "schema": "DEV_INTEG_SPE_SCH"
-      },
-    ),
-)
 
 @dag(
     schedule_interval="@daily",
@@ -33,22 +14,32 @@ dbt_snowflake = ProfileConfig(
     catchup=False,
     tags=["shopee"],
 )
-def dag_shopee_finance_transactions() -> None:
-    """
-    The simplest example of using Cosmos to render a dbt project as a TaskGroup.
-    """
-    pre_dbt = EmptyOperator(task_id="pre_dbt")
+def dag_shopee_finance_transactions():
+
+    dbt_snowflake = ProfileConfig(
+        profile_name="dbt_snowflake",
+        target_name="dev",
+        profile_mapping=SnowflakeUserPasswordProfileMapping(
+            conn_id="dbt_snowflake",
+            profile_args={
+                "database": "DEV_DB",
+                "schema": "DEV_INTEG_SPE_SCH"
+            },
+        ),
+    )
+    
+    execution_config = ExecutionConfig(
+        dbt_executable_path=DBT_EXECUTABLE,
+    )
 
     shopee_finance_transactions = DbtTaskGroup(
         group_id="shopee_finance_transactions",
         project_config=ProjectConfig(GDEC_DBT_PATH),
         profile_config=dbt_snowflake,
-        execution_config=venv_execution_config,
+        execution_config=execution_config,
     )
 
-    post_dbt = EmptyOperator(task_id="post_dbt")
-
-    pre_dbt >> shopee_finance_transactions >> post_dbt
+    return shopee_finance_transactions
 
 
 dag_shopee_finance_transactions()
