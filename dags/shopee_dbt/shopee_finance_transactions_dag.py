ab41564a0653735a912392e899c24a12d36acb22
Alvin <a.nollora@greatdealscorp.com>
2024-04-11 10:29:15 +0800

    📦 NEW:  variables and target

--- dags/shopee_dbt/shopee_finance_transactions_dag.py
+++ dags/shopee_dbt/shopee_finance_transactions_dag.py
@@ -1,8 +1,9 @@
-
+import os
 from datetime import datetime
 from pathlib import Path
 
 from airflow.decorators import dag
+from airflow.models import Variable
 
 from cosmos import DbtTaskGroup, ExecutionConfig, ProjectConfig, ProfileConfig
 from cosmos.profiles import SnowflakeUserPasswordProfileMapping
@@ -10,6 +11,8 @@ from cosmos.profiles import SnowflakeUserPasswordProfileMapping
 
 gdec_dbt_path = Path("/opt/airflow/dbt/gdec_dbt")
 dbt_executable = Path("/opt/airflow/dbt_venv/bin/dbt")
+target = Variable.get("target")
+profile_yml_path = "/opt/airflow/dbt/gdec_dbt/profiles.yml"
 
 venv_execution_config = ExecutionConfig(
     dbt_executable_path=str(dbt_executable),
@@ -17,14 +20,8 @@ venv_execution_config = ExecutionConfig(
 
 dbt_snowflake = ProfileConfig(
     profile_name="dbt_snowflake",
-    target_name="dev",
-    profile_mapping=SnowflakeUserPasswordProfileMapping(
-        conn_id="dbt_snowflake",
-        profile_args={
-            "database": "DEV_DB",
-            "schema": "DEV_INTEG_SPE_SCH"
-      },
-    ),
+    target_name=target,
+    profiles_yml_filepath=profile_yml_path,
 )
 
 @dag(
